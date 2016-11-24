137702a4753951de952ab3f890302d331ac0ad8e
Brad Adams <hi@breadadams.com>
2016-11-24 02:27:19 +0100

    HorizontalScrollBlocks updates
    + Code cleanup
    + Add functionality for content below image
    + Check content type, set round corners for artists

--- app/components/HorizontalScrollBlocks.js
+++ app/components/HorizontalScrollBlocks.js
@@ -14,7 +14,7 @@ export default class HorizontalScrollBlocks extends Component {
 		super()
 	}
 
-	backgroundImage(item) {
+	_backgroundImage(item) {
 
 		let itemImg
 
@@ -29,17 +29,32 @@ export default class HorizontalScrollBlocks extends Component {
 		return(
 			<Image
 				source={{uri: itemImg}}
-				style={styles.listItemImg}/>
+				style={StyleSheet.absoluteFill}/>
 		)
 	}
 
-	listItemTitle(title) {
+	_overlayTitle(title) {
 		if ( this.props.displayTitles && title ) {
 			return(
 				<Text
-					style={styles.listItemTitle}>
+					style={styles.listItemImgOverlayTitle}>
 					{title}
 				</Text>
+			)
+		}
+	}
+
+	_bottomContent(title) {
+		if ( this.props.displayContentBelow && title ) {
+			return(
+				<View
+					style={styles.bottomContent}>
+					<Text
+						style={styles.bottomContentTitle}
+						numberOfLines={1}>
+						{title}
+					</Text>
+				</View>
 			)
 		}
 	}
@@ -47,68 +62,101 @@ export default class HorizontalScrollBlocks extends Component {
 	renderListItem(item) {
 		return(
 			<View
-				style={styles.scrollListItem}>
-				{this.backgroundImage(item)}
-				{this.listItemTitle(item.name)}
+				style={styles.listItem}>
+				<View
+					style={[
+						styles.listItemImgWrap,
+						item.type == 'artist' ? styles.listItemImgWrapRound : null
+					]}>
+					{this._backgroundImage(item)}
+					{this._overlayTitle(item.name)}
+				</View>
+				{this._bottomContent(item.name)}
 			</View>
 		)
 	}
 
 	render() {
-		return(
-			<View
-				style={styles.rowWrap}>
+		if ( this.props.dataSource ) {
+			return(
+				<View
+					style={[styles.rowWrap, this.props.style]}>
 
-				<Text
-					style={styles.rowTitle}>
-					{this.props.title}
-				</Text>
+					{( this.props.dataSource 
 
-				<ListView
-					contentContainerStyle={styles.horizontalScrollWrap}
-					dataSource={this.props.dataSource}
-					renderRow={(item) => {return this.renderListItem(item)}}
-					horizontal={true}/>
+						? <Text
+							style={styles.rowTitle}>
+							{this.props.title}
+						</Text>
 
-			</View>
-		)
+						: null
+
+					)}
+
+					<ListView
+						contentContainerStyle={styles.horizontalScrollWrap}
+						dataSource={this.props.dataSource}
+						renderRow={(item) => {return this.renderListItem(item)}}
+						horizontal={true}
+						showsHorizontalScrollIndicator={false}/>
+
+				</View>
+			)
+		} else {
+			return null
+		}
 	}
+}
+
+HorizontalScrollBlocks.propTypes = {
+	title: React.PropTypes.string,
+	dataSource: React.PropTypes.object,
+	displayTitles: React.PropTypes.bool,
+	displayContentBelow: React.PropTypes.bool,
+	style: React.PropTypes.oneOfType([
+	    React.PropTypes.object,
+	    React.PropTypes.number,
+    ])
 }
 
 const styles = StyleSheet.create({
 	rowWrap: {
-		paddingTop: 10,
+
 	},
 
 	rowTitle: {
+		color: 'white',
 		textAlign: 'center',
-		fontWeight: '300',
+		fontWeight: 'bold',
 		fontSize: 16,
-		letterSpacing: 2,
-		marginBottom: 15,
+		letterSpacing: .5,
+		marginBottom: 10,
 	},
 
 	horizontalScrollWrap: {
 		paddingLeft: 15,
 	},
 
-	scrollListItem: {
+	listItem: {
+		marginRight: 15,
+		width: 150,
+	},
+
+	listItemImgWrap: {
 		width: 150,
 		height: 150,
-		backgroundColor: 'green',
-		marginRight: 15,
 		backgroundColor: '#eaeaea',
 	},
 
-	listItemImg: {
-		position: 'absolute',
-		left: 0,
-		top: 0,
-		right: 0,
-		bottom: 0,
+	listItemImgWrapRound: {
+		width: 135,
+		height: 135,
+		margin: 10,
+		borderRadius: 67.5,
+		overflow: 'hidden',
 	},
 
-	listItemTitle: {
+	listItemImgOverlayTitle: {
 		position: 'absolute',
 		left: 0,
 		right: 0,
@@ -117,6 +165,18 @@ const styles = StyleSheet.create({
 		textAlign: 'center',
 		color: '#ffffff',
 		fontWeight: '500',
+	},
+
+	bottomContent: {
+		flex: 1,
+		paddingTop: 6,
+		paddingHorizontal: 10,
+	},
+
+	bottomContentTitle: {
+		color: 'white',
+		textAlign: 'center',
+		fontWeight: '600',
+		fontSize: 11,
 	}
 })
-
