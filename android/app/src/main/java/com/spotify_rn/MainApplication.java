4df159fd846185220b5c00b1fc3bf07fb43eb275
Brad Adams <hi@breadadams.com>
2016-11-24 02:31:07 +0100

    link react-native-vector-icons with android

--- android/app/src/main/java/com/spotify_rn/MainApplication.java
+++ android/app/src/main/java/com/spotify_rn/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.psykar.cookiemanager.CookieManagerPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new ReactVideoPackage(),
             new RNFetchBlobPackage(),
             new CookieManagerPackage()
